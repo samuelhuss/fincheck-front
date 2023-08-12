f5abb1bb8df28ea9c07bbec44eff62ca057c44d3
huss <samuelhussdb@gmail.com>
2023-08-12 18:30:00 -0300

    add validation with zod

---
+++ src/view/pages/Login/useLoginController.ts
@@ -0,0 +1,31 @@
+import {useForm} from 'react-hook-form'
+import { zodResolver} from '@hookform/resolvers/zod'
+import { z } from 'zod'
+//import { httpClient } from '../../../app/services/HttpClient'
+
+const schema = z.object({
+    email: z.string()
+        .nonempty('E-mail é obrigatório')
+        .email('Informe um e-mail válido'),
+    password: z.string()
+        .nonempty('Senha é obrigatória')
+        .min(8, 'Senha deve conter pelo menos 8 dígitos')
+})
+
+type FormData = z.infer<typeof schema>
+
+export function useLoginController() {
+    const { 
+        register,
+        handleSubmit: hookFormHandleSubmit,
+        formState: { errors },
+    } = useForm<FormData>( {
+        resolver: zodResolver(schema)
+    })
+
+    const handleSubmit = hookFormHandleSubmit(async (data) => {
+        //await httpClient.post('/auth/signin', data)
+    })
+
+    return {handleSubmit, register, errors}
+}
--- src/view/pages/Register/index.tsx
+++ src/view/pages/Register/index.tsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
 import { Button } from "../../components/Button";
 import { Input } from "../../components/Input";
+import { useRegisterController } from "./useRegisterController";
 
 export function Register(){
+    const { register, errors, handleSubmit} = useRegisterController()
     return (
         <>
         <header className="flex flex-col items-center gap-4 text-center">
@@ -18,14 +20,16 @@ export function Register(){
             </p>
         </header>
 
-        <form className="mt-[60px] flex flex-col gap-4">
-            <Input type="text" placeholder="Nome" name="name" />
-            <Input type="email" placeholder="E-mail" name="email" />
-            <Input type="password" placeholder="Senha" name="password" />
+        <form 
+            onSubmit={handleSubmit}
+            className="mt-[60px] flex flex-col gap-4">
+                <Input type="text" placeholder="Nome" error={errors.name?.message} {...register('name')} />
+                <Input type="email" placeholder="E-mail" error={errors.email?.message} {...register('email')} />
+                <Input type="password" placeholder="Senha" error={errors.password?.message} {...register('password')} />
 
-            <Button type="submit" className="mt-2">
-                Criar conta
-            </Button>
+                <Button type="submit" className="mt-2">
+                    Criar conta
+                </Button>
         </form>
         
     </>
